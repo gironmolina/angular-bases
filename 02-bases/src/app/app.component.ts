0a339b4db83e520550d2853d6f28d79c73d611b7
Huans Hirons <gironmolina@gmail.com>
2025-01-27 00:26:22 +0100

    changes

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
   standalone: false
 })
 export class AppComponent {
-  isCollapsed = false;
+  public title:string = 'Hello World';
 }
