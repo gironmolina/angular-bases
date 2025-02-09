2e70eab545191b529b4b88cb272d8bc0361519f0
Huans Hirons <gironmolina@gmail.com>
2025-02-09 16:23:22 +0100

    changes

--- 03-gifs-app/src/app/app.module.ts
+++ 03-gifs-app/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -10,6 +13,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+
     GifsModule,
     SharedModule
   ],
---
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+
+  private _tagHistory: string[] = [];
+  private apiKey: string = 'tX55dVVvLTE1hF65zu2EqXUNkZATtiM8';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor( private http: HttpClient ) { }
+
+  get tagsHistory():string[] {
+    return [...this._tagHistory];
+  }
+
+  private organizeHistory(tag: string){
+    tag = tag.toLowerCase();
+
+    if (this._tagHistory.includes(tag)) {
+      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag);
+    }
+
+    this._tagHistory.unshift(tag);
+    this._tagHistory = this._tagHistory.splice(0,10);
+  }
+
+  searchTag(tag: string):void{
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+      this.http.get(`${ this.serviceUrl }/search`, { params })
+      .subscribe( resp =>{
+        console.log(resp);
+      });
+  }
+}
