637f6a8a168a2409e2db0b6a71a20acdb5337c50
Huans Hirons <gironmolina@gmail.com>
2025-02-09 10:56:58 +0100

    changes

---
+++ 03-gifs-app/src/app/shared/components/sidebar/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+  standalone: false
+})
+export class SidebarComponent { }
