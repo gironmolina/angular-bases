2fbf975db71c1769cfef8ac33db72f1c11e128bc
Huans Hirons <gironmolina@gmail.com>
2025-02-11 23:45:55 +0100

    changes

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -9,7 +9,9 @@ export class GifsService {
   private apiKey: string = 'tX55dVVvLTE1hF65zu2EqXUNkZATtiM8';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient ) {
+    this.loadLocalStorage();
+   }
 
   get tagsHistory():string[] {
     return [...this._tagHistory];
@@ -24,6 +26,19 @@ export class GifsService {
 
     this._tagHistory.unshift(tag);
     this._tagHistory = this._tagHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void{
+    localStorage.setItem('history', JSON.stringify(this._tagHistory));
+  }
+
+  private loadLocalStorage():void{
+    if (!localStorage.getItem('history')) return;
+    this._tagHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagHistory.length === 0) return;
+    this.searchTag(this._tagHistory[0]);
   }
 
   searchTag(tag: string):void{
