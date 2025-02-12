796669502024253f5e04544c26ac8711d525c8d0
Huans Hirons <gironmolina@gmail.com>
2025-02-12 01:04:00 +0100

    changes

---
+++ 03-gifs-app/src/app/shared/components/lazyImage/lazyImage.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  standalone: false,
+  templateUrl: './lazyImage.component.html'
+})
+export class LazyImageComponent { }
