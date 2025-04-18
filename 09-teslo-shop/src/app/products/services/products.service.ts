d74bafc37e625904d0d4a54b03a309788991467a
Huans Hirons <gironmolina@gmail.com>
2025-04-19 00:11:42 +0200

    changes

---
+++ 09-teslo-shop/src/app/products/services/products.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ProductsResponse } from '@products/interfaces/product.interface';
+import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
+
+interface Options {
+  limit?: number;
+  offset?: number;
+  gender?: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class ProductsService {
+  private readonly http = inject(HttpClient);
+
+  getProducts(Options: Options): Observable<ProductsResponse> {
+    return this.http
+      .get<ProductsResponse>(`${baseUrl}/products`)
+      .pipe(tap((resp) => console.log(resp)));
+  }
+}
